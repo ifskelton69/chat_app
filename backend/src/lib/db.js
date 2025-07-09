9bb68a0ce6134471d0f7a605cd41ec7eb7be7f9c
ifskelton69 <adityabirajdar0305@gmail.com>
2025-07-09 14:33:41 +0530

    cookie and token are set

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 export const connectdb = async () => {
     try {
         const con = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`Mongodb connected : ${con.connection.host}`);
+        console.log(`Mongodb connected : ${con.connection.host}`);//connect to the database
 
     } catch (error) {
         console.log(`Mongodb connected :`, error.message);
