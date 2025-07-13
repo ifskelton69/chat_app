4425627735abb30d1cf868c0b4a17f8a9568220b
ifskelton69 <adityabirajdar0305@gmail.com>
2025-07-13 15:32:22 +0530

    frontend little bit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import { Routes,Route } from 'react-router-dom'
+import homePage from './pages/homePage.jsx'
+import settingsPage from './pages/settingsPage.jsx'
+import loginPage from './pages/loginPage.jsx'
+import signUpPage from './pages/signUpPage.jsx'
+import profilePage from './pages/profilePage.jsx' 
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div >
+      <Navbar/>
+      <Routes>
+        <Route path='/'element= {<homePage/>}/>
+        <Route path='/signup'element= {<signUpPage />}/>
+        <Route path='/login'element= {<loginPage/>}/>
+        <Route path='/settings'element= {<settingsPage/>}/>
+        <Route path='/profile'element= {<progilePage/>}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+import daisyio from 'daisyui'
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyio],
+}
+
